2d10d7677e09f182ad78fd399846d55a92c91464
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-08-19 14:11:22 -0400

    implemented cors in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import app from './app';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
+
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+  }
+));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
