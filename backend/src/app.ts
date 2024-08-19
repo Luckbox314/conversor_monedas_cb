99343e87b5a54426241be54fb28a26a181746744
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-08-19 14:30:29 -0400

    fixed cors implementation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import conversionRoutes from './routes/conversionRoutes';
 import { errorHandler } from './middlewares';
 
 const app = express();
+
+app.use(cors(
+    {
+      origin: 'http://localhost:3000',
+    }
+));
 
 app.use(express.json());
 app.use('/api', conversionRoutes);
