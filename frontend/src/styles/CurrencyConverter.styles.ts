4890877ec390aaf96f844ad9c48ea0e303f30117
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-08-19 03:27:04 -0400

    added colors

--- frontend/src/styles/CurrencyConverter.styles.ts
+++ frontend/src/styles/CurrencyConverter.styles.ts
@@ -19,17 +19,23 @@ export const StyledSubmitButton = styled.a<{send: boolean}>`
   align-items: center;
   cursor: pointer;
   border-radius: 4px;
-  
+
 
   ${({ send }) =>
     send &&
     css`
       background-color: ${({ theme }) => theme.colors.primaryHighlight};
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.primaryHighlightLight};
+      }
     `
     ||
     !send &&
     css`
       background-color: ${({ theme }) => theme.colors.secondaryHighlight};
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.secondaryHighlightLight};
+      }
     `
   }
 `;
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -3,9 +3,13 @@ import { DefaultTheme } from 'styled-components';
 const theme: DefaultTheme = {
   colors: {
     primary: '#224E6E',
+    primaryLight: '#6E92AC',
     secondary: '#97ADBB',
+    secondaryLight: '#AFCADC',
     primaryHighlight: '#EDB44E',
+    primaryHighlightLight: '#EBD5AD',
     secondaryHighlight: '#5BABAB',
+    secondaryHighlightLight: '#A3DBDB',
     background: '#fff',
     backgroundDark: '#E7EDF0',
     lightText: '#fff',
